f3c7f85e22c94b5706ec5d958eed51b481459fb8
Darshil Aslaliya <68240703+darsh-22@users.noreply.github.com>
2021-05-13 17:47:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
+# ==================================================== #
+#                                                      #
+#  Created By: Darhil Aslaliya -  GitHub :- darsh-22   #
+#                                                      #
+# ==================================================== #
+
+# required module
 import random
-randNumber = random.randint(1, 10)
+
+# generate random number from 1 to 10
+randon_number = random.randint(1, 10)
 userGuess = None
 guesses = 0
 
-while(userGuess != randNumber):
+# main loop
+while(userGuess != randon_number):
     userGuess = int(input("Enter your guess: "))
     guesses += 1
-    if(userGuess==randNumber):
+    if(userGuess==randon_number):
         print("You guessed it right!")
     else:
-        if(userGuess>randNumber):
+        if(userGuess>randon_number):
             print("You guessed it wrong! Enter a smaller number")
         else:
             print("You guessed it wrong! Enter a larger number")
 
 print(f"You guessed the number in {guesses} guesses")
-with open("hiscore.txt", "r") as f:
-    hiscore = int(f.read())
 
-if(guesses<hiscore):
+
+# saving high score in file
+with open("highscore.txt", "r") as f:
+    highscore = int(f.read())
+
+# check if high score is break or not
+if(guesses<highscore):
     print("You have just broken the high score!")
-    with open("hiscore.txt", "w") as f:
+    with open("highscore.txt", "w") as f:
         f.write(str(guesses))
